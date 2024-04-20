45b3f235f73025826f6a53921849c3ddb2419aa2
helios12 <regawq@gmail.com>
2024-04-20 20:47:50 +0200

    Limitation: Q-Q plots to be calculated only for numeric columns

--- DataScienceHelpers/ExploratoryDataAnalysis.py
+++ DataScienceHelpers/ExploratoryDataAnalysis.py
@@ -3,10 +3,9 @@ import plotly.figure_factory as ff
 import plotly.graph_objects as go
 import seaborn as sns
 import warnings
+from pandas.api.types import is_numeric_dtype
 from plotly.subplots import make_subplots
 from scipy import stats
-
-warnings.simplefilter(action='ignore', category=FutureWarning)
 
 
 def display_histograms(data, cols=3, row_height=250):
@@ -71,11 +70,14 @@ def display_qqplot_histograms(data):
     Args:
         data (DataFrame): Dataframe for which series the Q-Q plots and histograms will be displayed.
     """
+    warnings.simplefilter(action='ignore', category=FutureWarning)
+
     for i in range(data.shape[1]):
         plt.subplots(1, 2, figsize=(8, 3))
 
-        plt.subplot(1, 2, 1)
-        stats.probplot(data[data.columns[i]], plot=plt)
+        if is_numeric_dtype(data[data.columns[i]]):
+            plt.subplot(1, 2, 1)
+            stats.probplot(data[data.columns[i]], plot=plt)
 
         plt.subplot(1, 2, 2)
         ax = sns.histplot(data=data, x=data.columns[i], kde=True, line_kws={'lw': 1, 'ls': '-'})
