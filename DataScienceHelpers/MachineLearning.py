df5992a2718ed52bbf54706bc757cd65b798c2ab
helios12 <regawq@gmail.com>
2024-05-30 08:52:33 +0200

    Added display of classification metrics

--- DataScienceHelpers/MachineLearning.py
+++ DataScienceHelpers/MachineLearning.py
@@ -1,7 +1,21 @@
 from sklearn import metrics
 
-def display_metrics(y, y_pred):
-    """Display the following metrics for the true and predicted y: R2, MAE, MAPE and MSE.
+
+def display_classification_metrics(y, y_pred):
+    """Display the following classification task metrics: Accuracy, Precision, Recall, F1.
+
+    Args:
+        y (Series): True y value.
+        y_pred (Series): Predicted y value.
+    """
+    print(f'Accuracy: {metrics.accuracy_score(y, y_pred):.2f}')
+    print(f'Precision: {metrics.precision_score(y, y_pred):.2f}')
+    print(f'Recall: {metrics.recall_score(y, y_pred):.2f}')
+    print(f'F1: {metrics.f1_score(y, y_pred):.2f}')
+    
+    
+def display_regression_metrics(y, y_pred):
+    """Display the following regression metrics: R2, MAE, MAPE and MSE.
 
     Args:
         y (Series): True y value.
