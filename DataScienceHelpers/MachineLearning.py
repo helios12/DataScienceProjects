6a640fbfbef819e3aeffdcafd917c931ede9d6ff
helios12 <regawq@gmail.com>
2024-06-17 07:47:51 +0200

    Added a method to plot a learning curve

--- DataScienceHelpers/MachineLearning.py
+++ DataScienceHelpers/MachineLearning.py
@@ -1,4 +1,8 @@
+import matplotlib as plt
+import numpy as np
+
 from sklearn import metrics
+from sklearn import model_selection
 
 
 def display_classification_metrics(y, y_pred):
@@ -25,3 +29,38 @@ def display_regression_metrics(y, y_pred):
     print(f'MAE: {metrics.mean_absolute_error(y, y_pred):.0f}')
     print(f'MAPE: {metrics.mean_absolute_percentage_error(y, y_pred) * 100:.0f}')
     print(f'MSE: {metrics.mean_squared_error(y, y_pred):.0f}')
+    
+
+def plot_learning_curve(model, X, y, cv, scoring="f1", ax=None, title=""):
+    """Plot a learning curve for a dataset unsing KFold cross validation.
+
+    Args:
+        model : Trained machine learning model.
+        X (matrix): Observation matrix.
+        y (vector): Target feature vector.
+        cv : Cross validatorl
+        scoring (str, optional): Scoring metric. Defaults to "f1".
+        ax (array of axes, optional): Matplotlib array of axes. Defaults to None.
+        title (str, optional): Diagram title. Defaults to "".
+    """
+    train_sizes, train_scores, valid_scores = model_selection.learning_curve(
+        estimator=model, 
+        X=X,  
+        y=y,  
+        cv=cv,
+        scoring=scoring,
+    )
+
+    train_scores_mean = np.mean(train_scores, axis=1)
+    valid_scores_mean = np.mean(valid_scores, axis=1)
+
+    if ax is None:
+        fig, ax = plt.subplots(figsize=(10, 4)) 
+    ax.plot(train_sizes, train_scores_mean, label="Train")
+    ax.plot(train_sizes, valid_scores_mean, label="Valid")
+    ax.set_title("Learning curve: {}".format(title))
+    ax.set_xlabel("Train data size")
+    ax.set_ylabel("Score")
+    ax.xaxis.set_ticks(train_sizes)
+    ax.set_ylim(0, 1)
+    ax.legend()
